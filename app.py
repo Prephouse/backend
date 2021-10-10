74879a0d015a248de764f35dc8027c641b6d2705
Jadon Fan <jdsare@gmail.com>
2021-10-10 15:53:03 -0400

    create models

--- app.py
+++ app.py
@@ -1,10 +1,53 @@
+import uuid
+from enum import Enum
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID, INT4RANGE
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/prephouse'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    firebase_token = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    uploads = db.relationship('Upload', backref='user', lazy=True)
+
+
+class Upload(db.Model):
+    class Category(Enum):
+        INTERVIEW = 0
+        PRESENTATION = 1
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    category = db.Column(db.Enum(Category), nullable=False)
+    date_uploaded = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    date_modified = db.Column(db.DateTime, server_onupdate=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    feedbacks = db.relationship('Feedback', backref='upload', lazy=True)
+
+
+class Feedback(db.Model):
+    class Type(Enum):
+        PAUSE = 0
+        SENTIMENT = 1
+        # TODO complete
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    type = db.Column(db.Enum(Type), nullable=False)
+    text = db.Column(db.Text)
+    score = db.Column(db.Numeric(10, 2), nullable=False)
+    time_range = db.Column(INT4RANGE(), nullable=False)
+    user_report = db.Column(db.Text)
+    upload_id = db.Column(UUID(as_uuid=True), db.ForeignKey('upload.id'), nullable=False)
 
 
 @app.route("/")
@@ -13,4 +56,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
