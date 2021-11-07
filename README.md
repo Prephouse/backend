132cab7b4e91c5d234d5a2c521a4bc3b61fc98dd
Jadon Fan <jdsare@gmail.com>
2021-11-07 14:30:36 -0500

    improve setup script

--- README.md
+++ README.md
@@ -8,8 +8,6 @@
 2. Make sure Docker is running
 3. Open a tab, in the root directory of this project, on your command line interface (CLI)
 4. Run `./setup.sh` on your CLI
-5. Run `docker-compose up --build` on your CLI to build and start the local database session and local development server
-6. Run `docker exec -it backend-prephouse-1 python3 create_schema.py` in a **separate** tab on your CLI
 
 ## Server Startup
 1. Make sure Docker is running
--- setup.sh
+++ setup.sh
@@ -1,3 +1,58 @@
 #!/usr/bin/env bash
 
+down=false
+mock=false
+up=false
+
+iter_cnt=0
+res=-1
+
+display_help() {
+  echo "
+    USAGE: ./setup.sh [-d] [-h] [-m] [-u]
+    -------------------------------------------------------------------------------------------------------------------------
+    |  FLAG  |  DESCRIPTION                                          |  ARGUMENT                  |  DEFAULT                |
+    |--------|-------------------------------------------------------|----------------------------|-------------------------|
+    |  -d    |  remove existing docker containers first, if any      |  N/A                       |  false                  |
+    |  -h    |  display help message                                 |  N/A                       |  false                  |
+    |  -m    |  insert test data into prephouse database             |  N/A                       |  false                  |
+    |  -u    |  start container immediately after setup completion   |  N/A                       |  false                  |
+    -------------------------------------------------------------------------------------------------------------------------
+  "
+}
+
+while getopts ":dhms" FLAG
+do
+  case $FLAG in
+    d) down=true;;
+    m) mock=true;;
+    s) up=true;;
+    h) display_help; exit 0;;
+    \?) echo "ERROR: One or more flags are not recognized by this script"; display_help; exit 1;;
+  esac
+done
+
+if [ $down = true ]; then
+  docker-compose down --remove-orphans
+fi
+
 docker-compose build
+docker-compose up --detach && {
+  sleep 10
+  until [ $iter_cnt -gt 0 ] && [ $res = 0 ]
+  do
+    ((iter_cnt++))
+    if [ $mock = true ]; then
+      docker exec -it backend-prephouse-1 python3 create_schema.py --mock
+    else
+      docker exec -it backend-prephouse-1 python3 create_schema.py
+    fi
+    res=$?
+    sleep 3
+  done
+}
+docker-compose stop  # stop regardless of the [-u] flag
+
+if [ $up = true ]; then
+  docker-compose up --no-recreate
+fi
