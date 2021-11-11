5f4858e4c68471e6cf0faf94c4bab5438827807e
Chandler Lei <chandlerlei87@gmail.com>
2021-11-10 19:07:52 -0500

    Set up gRPC

---
+++ api/analyze.py
@@ -0,0 +1,31 @@
+from flask import abort, jsonify, request, Blueprint
+from app import Feedback, app
+
+import os
+import grpc
+from prephouse_pb2 import Video
+from prephouse_pb2_grpc import PrephouseEngineStub
+
+analyze_api = Blueprint('analyze_api', __name__)
+
+@app.route("/analyze")
+def analyze_upload():
+    upload_link = request.args.get('upload_link', type=str)
+
+    with grpc.insecure_channel(f"{os.environ['ENGINE_GRPC_IP']}:{os.environ['ENGINE_GRPC_PORT']}") as channel:
+        stub = PrephouseEngineStub(channel)
+        feedback = stub.GetFeedback(Video(link=upload_link))
+
+        # TODO: Save Feedback into DB
+
+        return jsonify(
+            [
+                {
+                    "type": f.type,
+                    "text": f.text,
+                    "score": f.score,
+                    "time_start": f.timeStart,
+                    "time_end": f.timeEnd
+                } for f in feedback
+            ]
+        )
