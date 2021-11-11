afb484191f2ac8f384f3b34fee8cbfffae61200d
Jadon Fan <jdsare@gmail.com>
2021-11-11 12:12:35 -0500

    refactor code

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ from psycopg2.extras import NumericRange
 
 
 def get_integral_numeric_range_bounds(nr: NumericRange) -> [int, int]:
-    p = re.compile(r'\[(\d+), (\d+)\)')
+    p = re.compile(r"\[(\d+), (\d+)\)")
     m = p.match(str(nr))
     return int(m.group(1)), int(m.group(2))
