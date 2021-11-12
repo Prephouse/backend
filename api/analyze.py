2f209b08ee9325eb1e11c6de2f7cab8b09a3ce0d
Jadon Fan <jdsare@gmail.com>
2021-11-11 20:07:52 -0500

    fix linter

--- api/analyze.py
+++ api/analyze.py
@@ -1,18 +1,22 @@
-from flask import abort, jsonify, request, Blueprint
-from app import Feedback, app
-
 import os
+
 import grpc
+from flask import jsonify, request, Blueprint
+
+from app import app
 from prephouse_pb2 import Video
 from prephouse_pb2_grpc import PrephouseEngineStub
 
-analyze_api = Blueprint('analyze_api', __name__)
+analyze_api = Blueprint("analyze_api", __name__)
+
 
 @app.route("/analyze")
 def analyze_upload():
-    upload_link = request.args.get('upload_link', type=str)
+    upload_link = request.args.get("upload_link", type=str)
 
-    with grpc.insecure_channel(f"{os.environ['ENGINE_GRPC_IP']}:{os.environ['ENGINE_GRPC_PORT']}") as channel:
+    with grpc.insecure_channel(
+        f"{os.environ['ENGINE_GRPC_IP']}:{os.environ['ENGINE_GRPC_PORT']}"
+    ) as channel:
         stub = PrephouseEngineStub(channel)
         feedback = stub.GetFeedback(Video(link=upload_link))
 
@@ -25,7 +29,8 @@ def analyze_upload():
                     "text": f.text,
                     "score": f.score,
                     "time_start": f.timeStart,
-                    "time_end": f.timeEnd
-                } for f in feedback
+                    "time_end": f.timeEnd,
+                }
+                for f in feedback
             ]
         )
