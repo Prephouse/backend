7bd56ddb314d1d176467fd29c1cedd9e7e2a79a8
Jadon Fan <jdsare@gmail.com>
2021-12-17 22:49:43 -0500

    fix

--- prephouse/schemas/feedback_schema.py
+++ prephouse/schemas/feedback_schema.py
@@ -8,7 +8,7 @@ class FeedbackRequestSchema(Schema):
     upload_ids = fields.List(fields.Int, dump_default=None)
     time_start = fields.Int(dump_default=0)
     time_end = fields.Int(dump_default=constants.PSQL_INT_MAX)
-    category = fields.Int(required=True, validate=validate.OneOf(list(map(int, Feedback.Feature))))
+    category = fields.Int(validate=validate.OneOf(list(map(int, Feedback.Feature))))
 
 
 class FeedbackResponseSchema(Schema):
