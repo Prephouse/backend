573d98917951819173f20b21f7aad4b3c6e51520
Mivia Li <32752716+Mivialll@users.noreply.github.com>
2022-02-15 10:24:58 -0500

    Refactor and Fix User Performance APIs (#102)
    Update the API to produce outputs as expected

--- prephouse/api/user_progress_tracking.py
+++ prephouse/api/user_progress_tracking.py
@@ -1,115 +1,77 @@
+import json
+
 from flask import Blueprint, abort, jsonify, request
 
 from prephouse.decorators.authentication import private_route
 from prephouse.models import Feedback, Upload, UploadQuestion
 from prephouse.schemas.user_progress_tracking_schema import (
+    user_progress_tracking_overall_score_session_response,
     user_progress_tracking_question_request,
-    user_progress_tracking_response,
+    user_progress_tracking_question_response,
     user_progress_tracking_session_request,
+    user_progress_tracking_session_response,
 )
 
 user_progress_tracking_api = Blueprint(
     "user_progress_tracking", __name__, url_prefix="/user_progress_tracking"
 )
 
-
+# The score for all features for all questions for all sessions
+# Session >> Question >> Feature Score
 @user_progress_tracking_api.get("feature_per_question/")
 @private_route
-def get_score_per_feature_per_question():
+def get_score_per_feature_per_question_per_session():
     if validation_errors := user_progress_tracking_question_request.validate(request.args):
         abort(422, validation_errors)
-    question_id = request.args.get("question_id")
+    session_id = request.args.get("session_id")
 
-    query = UploadQuestion.query
-    if question_id:
-        query = query.filter_by(question_id=question_id)
+    upload_questions = UploadQuestion.query.filter_by(upload_id=session_id)
+    scores = [
+        (feedback.category.name, float(feedback.result), upload_question.question_id)
+        for upload_question in upload_questions
+        for feedback in upload_question.feedbacks
+    ]
 
-    # query all feedbacks of one question in a session
-    response = []
-    query = query.join(Feedback).add_columns(
-        Feedback.id,
-        Feedback.category,
-        Feedback.result,
-    )
-
-    for feature in Feedback.FeedbackCategory:
-        sub_query = query.filter_by(category=feature, subcategory="score")
-        feedback = sub_query.first()
-
-        if feedback:
-            item = {
-                "id": feedback.id,
-                "score": float(feedback.result),
-            }
-            response.append(item)
-    return jsonify(user_progress_tracking_response.dump(response))
+    return {"session_id": session_id, "response_data": json.dumps(scores)}
 
 
+# Returns a total score for each feature for a given session
+# Session >> Feature Score Averages
 @user_progress_tracking_api.get("feature_per_session/")
 @private_route
-def get_overall_score_per_feature_per_session():
+def get_score_per_feature_per_session():
     if validation_errors := user_progress_tracking_session_request.validate(request.args):
         abort(422, validation_errors)
     session_id = request.args.get("session_id")
 
-    # query all feedbacks of all questions in a session
-    query = Upload.query.filter_by(id=session_id)
-    query = (
-        query.join(UploadQuestion, Upload.id == UploadQuestion.upload_id)
-        .join(Feedback, UploadQuestion.id == Feedback.uq_id)
-        .add_columns(
-            Feedback.id,
-            Feedback.category,
-            Feedback.subcategory,
-            Feedback.result,
-        )
-    )
+    upload_questions = UploadQuestion.query.filter_by(upload_id=session_id)
+    scores = [
+        (feedback.category.name, float(feedback.result), upload_question.question_id)
+        for upload_question in upload_questions
+        for feedback in upload_question.feedbacks
+    ]
 
-    response = []
-    feedbacks = query.filter_by(subcategory="score")
+    feature_scores_dict = {}
+    for feature_name in Feedback.FeedbackCategory:
+        feature_scores_dict[feature_name.name] = 0
+    for score_data in scores:
+        feature_scores_dict[score_data[0]] += score_data[1]
 
-    for feedback in feedbacks.all() or []:
-        # feature_scores[feedback.category] += float(feedback.result)
-        item = {
-            "id": feedback.id,
-            "score": float(feedback.result),
-        }
-        response.append(item)
-    return jsonify(user_progress_tracking_response.dump(response))
+    return {"session_id": session_id, "response_data": json.dumps(feature_scores_dict)}
 
 
-@user_progress_tracking_api.get("overall_session/")
+# The overall score for all sessions
+# Session >> Overall Score
+@user_progress_tracking_api.get("overall_per_session/")
 @private_route
-def get_overall_score_per_all_feature_per_session():
+def get_overall_score_per_session():
     if validation_errors := user_progress_tracking_session_request.validate(request.args):
         abort(422, validation_errors)
     session_id = request.args.get("session_id")
-    feature_scores = dict.fromkeys(Feedback.FeedbackCategory, 0.0)
 
-    # query all feedbacks of all questions in a session
     query = Upload.query.filter_by(id=session_id)
-    query = (
-        query.join(UploadQuestion, Upload.id == UploadQuestion.upload_id)
-        .join(Feedback, UploadQuestion.id == Feedback.uq_id)
-        .add_columns(
-            Upload.id,
-            Feedback.category,
-            Feedback.subcategory,
-            Feedback.result,
-        )
-    )
-
-    response = []
-    query = query.filter_by(subcategory="score")
-    for feature in Feedback.FeedbackCategory:
-        feedbacks = query.filter_by(category=feature).all()
-        for feedback in feedbacks or []:
-            feature_scores[feature] += float(feedback.result)
-
-        if feedbacks:
-            item = {
-                "id": session_id,
-                "score": feature_scores[feature] / len(feedbacks),
-            }
-            response.append(item)
-    return jsonify(user_progress_tracking_response.dump(response))
+    response = query.first()
+    return {
+        "session_id": session_id,
+        "response_data": response.score if response else -1,
+    }
--- prephouse/schemas/user_progress_tracking_schema.py
+++ prephouse/schemas/user_progress_tracking_schema.py
@@ -2,18 +2,35 @@ from marshmallow import Schema, fields
 
 
 class UserProgressTrackingQuestionRequestSchema(Schema):
-    question_id = fields.Int(required=True)
+    session_id = fields.Str(required=True)
 
 
 class UserProgressTrackingSessionRequestSchema(Schema):
     session_id = fields.Str(required=True)
 
 
-class UserProgressTrackingResponseSchema(Schema):
-    id = fields.Str(required=True)
+class UserProgressTrackingQuestionResponseSchema(Schema):
+    session_id = fields.Str(required=True)
+    question_id = fields.Str(required=True)
+    score = fields.Str(required=True)
+    # score = fields.Dict(keys=fields.Int(), values=fields.Str())(required=True)
+
+
+class UserProgressTrackingSessionResponseSchema(Schema):
+    session_id = fields.Str(required=True)
+    score = fields.Str(required=True)
+    # score = fields.Dict(keys=fields.Int(), values=fields.Str())(required=True)
+
+
+class UserProgressTrackingOverallScoreSessionResponseSchema(Schema):
+    session_id = fields.Str(required=True)
     score = fields.Float(required=True)
 
 
 user_progress_tracking_question_request = UserProgressTrackingQuestionRequestSchema()
 user_progress_tracking_session_request = UserProgressTrackingSessionRequestSchema()
-user_progress_tracking_response = UserProgressTrackingResponseSchema(many=True)
+user_progress_tracking_question_response = UserProgressTrackingQuestionResponseSchema()
+user_progress_tracking_session_response = UserProgressTrackingSessionResponseSchema(many=True)
+user_progress_tracking_overall_score_session_response = (
+    UserProgressTrackingOverallScoreSessionResponseSchema()
+)
