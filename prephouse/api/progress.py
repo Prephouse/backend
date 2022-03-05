df768b24d73c8995248c258bd13f6abbe3da1dbf
Chandler Lei <33561090+chandlerlei2017@users.noreply.github.com>
2022-03-05 17:36:11 -0500

    Initial Session Page  (#127)

--- prephouse/api/progress.py
+++ prephouse/api/progress.py
@@ -10,6 +10,7 @@ from prephouse.schemas.progress_schema import (
     category_scores_response_schema,
     progress_request_schema,
     progress_response_schema,
+    session_response_schema,
     session_scores_request_schema,
     session_scores_response_schema,
 )
@@ -17,21 +18,21 @@ from prephouse.schemas.progress_schema import (
 progress_api = Blueprint("progress", __name__, url_prefix="/progress")
 
 
-@progress_api.get("scores_per_category/")
+@progress_api.get("scores_by_feature/")
 @use_kwargs(session_scores_request_schema, location="query")
 @private_route
-def get_scores_per_feature_across_all_sessions():
+def get_scores_by_feature():
     """
     Get all the scores for a user per session, categorized by the feature.
 
     User >> Uploads >> Upload Questions >> Feedback
     """
     user_id = request.user.id
-    res = defaultdict(list)
+    res = {"dates": [], "scores": defaultdict(list)}
 
     for upload in Upload.query.filter_by(user_id=user_id).order_by(asc(Upload.date_uploaded)):
         res["dates"].append(upload.date_uploaded)
-        res["overall_scores"].append(float(upload.score or 0))
+        res["scores"]["overall_scores"].append(float(upload.score or 0))
         session_scores = {f.get_api_safe_feature_name(): 0 for f in Feedback.FeedbackCategory}
 
         query = (
@@ -55,15 +56,15 @@ def get_scores_per_feature_across_all_sessions():
             session_scores[q.category.get_api_safe_feature_name()] = q.avg_score
 
         for feature, score in session_scores.items():
-            res[f"{feature}_scores"].append(float(score))
+            res["scores"][f"{feature}_scores"].append(float(score))
 
     return jsonify(category_scores_response_schema.dump(res))
 
 
-@progress_api.get("scores_per_session/")
+@progress_api.get("scores_by_session/")
 @use_kwargs(session_scores_request_schema, location="query")
 @private_route
-def get_scores_per_session():
+def get_scores_by_session():
     """
     Get scores of all features for each session the user has.
 
@@ -96,38 +97,64 @@ def get_scores_per_session():
         scores_dict[feedback.id][3][feedback.feedback_category] = float(feedback.avg_score or 0)
 
     for upload_id, (date, upload_category, upload_score, scores) in scores_dict.items():
-        item = {
+        item = {"scores": {"overall_score": upload_score}}
+
+        item["scores"] |= {
             f"{f.get_api_safe_feature_name()}_score": scores[f] for f in Feedback.FeedbackCategory
         }
+
         item |= {
             "date": date,
             "session_category": upload_category.get_category_name(),
             "session_id": upload_id,
-            "overall_score": upload_score,
         }
         res.append(item)
 
     return jsonify(session_scores_response_schema.dump({"sessions": res}))
 
 
-@progress_api.get("feature_per_question/")
+@progress_api.get("scores_for_session/")
 @use_kwargs(progress_request_schema, location="query")
-@private_route
+# @private_route
 def get_score_per_feature_per_question_per_session(session_id):
     """
     Get the score for all features for all questions for a given session.
 
-    Session >> Question >> Feature Score
+    Upload >> Upload Questions >> Feedback
     """
-    upload_questions = UploadQuestion.query.filter_by(upload_id=session_id)
-    scores = [
-        (feedback.category.name, float(feedback.result), upload_question.question_id)
-        for upload_question in upload_questions
-        for feedback in upload_question.feedbacks
-    ]
+    scores_dict = {f: 0 for f in Feedback.FeedbackCategory}
 
-    res = {"session_id": session_id, "response_data": scores}
-    return jsonify(progress_response_schema.dump(res))
+    query = (
+        Upload.query.join(UploadQuestion, UploadQuestion.upload_id == Upload.id, isouter=True)
+        .join(Feedback, Feedback.uq_id == UploadQuestion.id, isouter=True)
+        .filter(Upload.id == session_id)
+        .group_by(Upload.id, Feedback.category)
+        .add_columns(
+            Upload.id,
+            Upload.date_uploaded,
+            Upload.score.label("upload_score"),
+            Upload.category.label("upload_category"),
+            Feedback.category.label("feedback_category"),
+            func.avg(Feedback.result).label("avg_score"),
+        )
+    ).all()
+
+    for feedback in query:
+        scores_dict[feedback.feedback_category] = float(feedback.avg_score or 0)
+
+    res = {"scores": {"overall_score": 0}}
+
+    if query:
+        res |= {
+            "session_category": query[0].upload_category.get_category_name(),
+            "date": query[0].date_uploaded,
+        }
+        res["scores"]["overall_score"] = query[0].upload_score
+
+    for f in Feedback.FeedbackCategory:
+        res["scores"][f"{f.get_api_safe_feature_name()}_score"] = scores_dict[f]
+
+    return jsonify(session_response_schema.dump(res))
 
 
 @progress_api.get("feature_per_session/")
--- prephouse/schemas/progress_schema.py
+++ prephouse/schemas/progress_schema.py
@@ -15,11 +15,8 @@ class SessionScoresRequestSchema(Schema):
     pass
 
 
-class SessionScoresResponseSchema(Schema):
-    class UploadEntrySchema(Schema):
-        session_id = fields.UUID(required=True)
-        session_category = fields.String(required=True)
-        date = fields.DateTime(required=True)
+class SessionResponseSchema(Schema):
+    class ScoresSchema(Schema):
         overall_score = fields.Float(required=True)
         silent_pauses_score = fields.Float(required=True)
         volume_score = fields.Float(required=True)
@@ -29,23 +26,34 @@ class SessionScoresResponseSchema(Schema):
         pitch_score = fields.Float(required=True)
         filler_words_score = fields.Float(required=True)
 
-    sessions = fields.List(fields.Nested(UploadEntrySchema), required=True)
+    session_id = fields.Str(required=True)
+    session_category = fields.String(required=True)
+    date = fields.DateTime(required=True)
+    scores = fields.Nested(ScoresSchema, required=True)
+
+
+class SessionScoresResponseSchema(Schema):
+    sessions = fields.List(fields.Nested(SessionResponseSchema), required=True)
 
 
 class CategoryScoresResponseSchema(Schema):
+    class ScoresSchema(Schema):
+        overall_scores = fields.List(fields.Float, required=True)
+        silent_pauses_scores = fields.List(fields.Float, required=True)
+        volume_scores = fields.List(fields.Float, required=True)
+        background_light_scores = fields.List(fields.Float, required=True)
+        gaze_direction_scores = fields.List(fields.Float, required=True)
+        emotion_scores = fields.List(fields.Float, required=True)
+        pitch_scores = fields.List(fields.Float, required=True)
+        filler_words_scores = fields.List(fields.Float, required=True)
+
     dates = fields.List(fields.DateTime, required=True)
-    overall_scores = fields.List(fields.Float, required=True)
-    silent_pauses_scores = fields.List(fields.Float, required=True)
-    volume_scores = fields.List(fields.Float, required=True)
-    background_light_scores = fields.List(fields.Float, required=True)
-    gaze_direction_scores = fields.List(fields.Float, required=True)
-    emotion_scores = fields.List(fields.Float, required=True)
-    pitch_scores = fields.List(fields.Float, required=True)
-    filler_words_scores = fields.List(fields.Float, required=True)
+    scores = fields.Nested(ScoresSchema, required=True)
 
 
 progress_request_schema = ProgressRequestSchema()
 progress_response_schema = ProgressResponseSchema()
+session_response_schema = SessionResponseSchema()
 session_scores_request_schema = SessionScoresRequestSchema()
 session_scores_response_schema = SessionScoresResponseSchema()
 category_scores_response_schema = CategoryScoresResponseSchema()
